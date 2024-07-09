67eb49259cb6ebe7d5433e4c01d232f624aa1640
suhoi <suhoio2k00@gmail.com>
2024-07-09 11:04:59 +0300

    init proj

---
+++ src/main/java/com/suhoi/WalletMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.suhoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/suhoi/WalletMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suhoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
